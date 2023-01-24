8d8c4e0298675fa146fa4171f5842eb49e0848e0
SuperGhostSama <hanaoui568@gmail.com>
2023-01-24 13:26:25 +0100

    tweaked the form validation so that it accepts space

--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -1,5 +1,5 @@
 function validateCategory() {
-    if (document.getElementById('category').value == '' || !/^[a-zA-Z]{5,}$/.test(document.getElementById('category').value)) {
+    if (document.getElementById('category').value == '' || !/^[a-zA-Z ]{5,}$/.test(document.getElementById('category').value)) {
         document.getElementById('category').setAttribute('style', 'color:red; border: 1px red solid ;');
   
         document.getElementById('validateCategory').innerText = 'Please enter a valid category! check that the title contains at least 5 characters and without special characters!!';
@@ -15,7 +15,7 @@ function validateCategory() {
 }
 
 function validateTitle() {
-    if (document.getElementById('title').value == '' || !/^[a-zA-Z]{5,}$/.test(document.getElementById('title').value)) {
+    if (document.getElementById('title').value == '' || !/^[a-zA-Z ]{5,}$/.test(document.getElementById('title').value)) {
         document.getElementById('title').setAttribute('style', 'color:red; border: 1px red solid ;');
   
         document.getElementById('validateTitle').innerText = 'Please enter a valid title! check that the title contains at least 5 characters and without special characters!!';
@@ -31,7 +31,7 @@ function validateTitle() {
 }
 
 function validateDescription() {
-    if (document.getElementById('description').value == '' || !/^[a-zA-Z0-9-]+$/.test(document.getElementById('description').value)) {
+    if (document.getElementById('description').value == '' || !/^[a-zA-Z0-9- ]+$/.test(document.getElementById('description').value)) {
         document.getElementById('description').setAttribute('style', 'color:red; border: 1px red solid ;');
   
         document.getElementById('validateDescription').innerText = 'Please enter a valid description! check that the description does not contain any special characters!!';
